beffdbd911a5b35d28c044b09e55e69da7da8103
Wesley Anemam <wesleylira@gmail.com>
2015-08-10 07:04:27 +0100

    Updating spec descriptions

--- spec/dbm-spec.js
+++ spec/dbm-spec.js
@@ -1,12 +1,12 @@
 var dbm, app = require('../index.js')
 
-describe('A dbm holds multiple database instances in memory', function(){
+describe('A dbm', function(){
 
 	beforeEach(function(){
 		dbm = new app.dbm()
 	})
 
-	it('creates dbs with the set method and retrives them with the get', function(){
+	it('creates and holds multiple database instances in memory with set/get methods', function(){
 		var dbs = ['my-db', 'my-db2', 'another-db']
 
 		for (var i = 0, length = dbs.length; i < length; i++)
@@ -27,7 +27,7 @@ describe('A dbm holds multiple database instances in memory', function(){
 		expect(dbm.get('_dbs')[db_name]).not.toBeDefined()
 	})
 
-	it('Returns all dbs with the key \'_dbs\'', function(){
+	it('returns all dbs if you invoke the get method with the key \'_dbs\'', function(){
 		expect(dbm.get('_dbs')).toBe({})
 
 		dbm.set('db1')
@@ -37,62 +37,60 @@ describe('A dbm holds multiple database instances in memory', function(){
 		expect(dbm.get('_dbs').db2).toBeDefined()
 	})
 
-	describe('Exceptions will be thrown in the following cases', function(){
-		it('throws an exception if a db is not defined and you try to get it', function(){
-			expect(function(){
-				dbm.set('deleted-db')
-				expect(dbm.get('deleted-db')).toBeDefined()
+	it('throws an exception if a db is not defined and you try to get it', function(){
+		expect(function(){
+			dbm.set('deleted-db')
+			expect(dbm.get('deleted-db')).toBeDefined()
 
-				dbm.del('deleted-db')
-			
-				dbm.get('deleted-db')
-			}).toThrow()
+			dbm.del('deleted-db')
+		
+			dbm.get('deleted-db')
+		}).toThrow()
 
-			expect(function(){
-				dbm.get('unexisting-db')
-			}).toThrow()
+		expect(function(){
+			dbm.get('unexisting-db')
+		}).toThrow()
 
-			expect(function(){
-				dbm.get(undefined)
-			}).toThrow()
+		expect(function(){
+			dbm.get(undefined)
+		}).toThrow()
 
-			expect(function(){
-				dbm.get()
-			}).toThrow()
-		})
+		expect(function(){
+			dbm.get()
+		}).toThrow()
+	})
 
-		it('throws an exception if you try to set a db without name', function(){
-			expect(function(){
-				dbm.set(undefined)
-			}).toThrow()
+	it('throws an exception if you try to set a db without name', function(){
+		expect(function(){
+			dbm.set(undefined)
+		}).toThrow()
 
-			expect(function(){
-				dbm.set()
-			}).toThrow()
-		})
+		expect(function(){
+			dbm.set()
+		}).toThrow()
+	})
 
-		it('will also throw exceptions if the name is not a string', function(){
-			expect(function(){
-				dbm.set(function(){})
-			}).toThrow()
+	it('will also throw exceptions if the name is not a string', function(){
+		expect(function(){
+			dbm.set(function(){})
+		}).toThrow()
 
-			expect(function(){
-				dbm.set(true)
-			}).toThrow()
+		expect(function(){
+			dbm.set(true)
+		}).toThrow()
 
-			expect(function(){
-				dbm.set(false)
-			}).toThrow()
+		expect(function(){
+			dbm.set(false)
+		}).toThrow()
 
-			expect(function(){
-				dbm.set(100)
-			}).toThrow()
-		})
+		expect(function(){
+			dbm.set(100)
+		}).toThrow()
+	})
 
-		it('also throws an exception if you try to set _dbs, since it is a reserved namespace', function(){
-			expect(function(){
-				dbm.set('_dbs')
-			}).toThrow()
-		})
+	it('also throws an exception if you try to set _dbs, since it is a reserved namespace', function(){
+		expect(function(){
+			dbm.set('_dbs')
+		}).toThrow()
 	})
 })
